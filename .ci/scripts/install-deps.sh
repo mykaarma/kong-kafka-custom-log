f0a928736aed75fcaaad68dc16ebb60940498912
AadiS11 <aadi.shukla@mykaarma.com>
2022-10-19 09:51:35 +0530

    Updated scripts

--- .ci/scripts/install-deps.sh
+++ .ci/scripts/install-deps.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-rockspec_file_name=$(ls *.rockspec)
-luarocks upload $rockspec_file_name --api-key=$1
+sudo apt-get install lua5.1 liblua5.1
+
+wget https://luarocks.org/releases/luarocks-3.9.1.tar.gz
+tar zxpf luarocks-3.9.1.tar.gz
+
+cd luarocks-3.9.1
+
+./configure && make && sudo make install
+
+sudo luarocks --version
+
+sudo luarocks install luasocket
